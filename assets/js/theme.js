dbd29f799073c459d7c67ee7e0426d2160bc0d27
Sk Arif <skarif2@gmail.com>
2018-08-08 18:01:12 +0600

    updated map style and team name css

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -286,15 +286,72 @@
         mapTypeControl: false,
         zoom: $zoom,
             styles: [
-    {
-        "featureType": "administrative.country",
-        "elementType": "geometry",
-        "stylers": [
+                {
+                    "featureType": "landscape.natural",
+                    "elementType": "geometry.fill",
+                    "stylers": [
                         {
-                            "visibility": "simplified"
+                            "visibility": "on"
                         },
                         {
-                            "hue": "#ff0000"
+                            "color": "#e0efef"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "hue": "#1900ff"
+                        },
+                        {
+                            "color": "#c0e8e8"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "lightness": 100
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.line",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "lightness": 700
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#7dcdcd"
                         }
                     ]
                 }
